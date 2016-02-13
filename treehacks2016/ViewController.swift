337de510cf69099b768cfd3ebb41e57dd3f86698
Michael Straka <michaelstraka@Michaels-MacBook-Pro-27.local>
2016-02-13 00:51:48 -0800

    Initial Commit

---
+++ treehacks2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  treehacks2016
+//
+//  Created by Michael Straka on 2/13/16.
+//  Copyright (c) 2016 Michael Straka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ treehacks2016Tests/treehacks2016Tests.swift
@@ -0,0 +1,36 @@
+//
+//  treehacks2016Tests.swift
+//  treehacks2016Tests
+//
+//  Created by Michael Straka on 2/13/16.
+//  Copyright (c) 2016 Michael Straka. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class treehacks2016Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
